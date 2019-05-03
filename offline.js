7a3acf4539c3c020fd04a4c912e23b3583ae000f
Kyle R. Olson <kylerolson@hotmail.com>
2019-05-02 23:23:27 -0600

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1556860890,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/player-sheet0.png",
+		"images/pixilframe0copy-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/pixilframe-sheet0.png",
+		"images/camera-sheet0.png",
+		"images/pixilframe2-sheet0.png",
+		"images/580b57fbd9996e24bc43bf-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/pixilframe3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
